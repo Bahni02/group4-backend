ef72073aa10b4f82a8f565881c890d9575ea1448
bahni <bahninfe98@gmail.com>
2021-11-22 16:24:33 +0530

    Prepare spring boot app for heroku

--- src/main/java/com/programming/techie/springredditclone/config/AppConfig.java
+++ src/main/java/com/programming/techie/springredditclone/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
 
-@Componentg
+@Component
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "app")
 public class AppConfig {
